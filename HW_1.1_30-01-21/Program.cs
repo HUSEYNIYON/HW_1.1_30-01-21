4040f5b85f9b3f7a63617a83b451bde4b1b5a5f4
Huseyn <huseyn19.burhonov1999@gmail.com>
2021-01-31 23:03:06 +0500

    Добавьте файлы проекта.

---
+++ HW_1.1_30-01-21/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW_1._1_30_01_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
